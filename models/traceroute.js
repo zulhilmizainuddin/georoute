cade836c739f6967f0345447b32b404e5ac87cf9
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-19 07:42:56 +0800

    Add empty traceroute wrapper

---
+++ models/traceroute.js
@@ -0,0 +1,15 @@
+const spawn = require('child_process').spawn;
+const events = require('events');
+const readline = require('readline');
+
+class Traceroute extends events.EventEmitter {
+    trace(domainName) {
+
+    }
+
+    static parseHop(hopData) {
+
+    }
+}
+
+module.exports = Traceroute;
