ee8d26d007ad48abdfb7b1a33ffdd1baaef55795
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-19 13:11:31 +0800

    Refactor ip2location

---
+++ config.js
@@ -0,0 +1,7 @@
+
+config = {};
+
+config.ipv4db = 'IP2LOCATION-LITE-DB5.BIN';
+config.ipv6db = 'IP2LOCATION-LITE-DB5.IPV6.BIN';
+
+module.exports = config;
--- models/ip2location.js
+++ models/ip2location.js
@@ -1,14 +1,36 @@
 const ip2loc = require('ip2location-nodejs');
 const path = require('path');
+const net = require('net');
+
+const config = require('../config');
 
 class Ip2Location {
+    constructor() {
+        this.connectedDb = '';
+    }
+
     query(ip) {
-        let database = (ip.match(/\d+\.\d+\.\d+\.\d+/) !== null) ? 'IP2LOCATION-LITE-DB5.BIN' : 'IP2LOCATION-LITE-DB5.IPV6.BIN';
-        database = path.join(__dirname, `../databases/${database}`);
+        let database = null;
 
-        ip2loc.IP2Location_init(database);
+        if (net.isIPv4(ip)) {
+            database = config.ipv4db;
+        }
+        else if(net.isIPv6(ip)) {
+            database = config.ipv6db;
+        }
 
-        const result = ip2loc.IP2Location_get_all(ip);
+        let result = null;
+        if (database !== null) {
+            if (this.connectedDb !== database) {
+                const databasePath = path.join(__dirname, `../databases/${database}`);
+
+                ip2loc.IP2Location_init(databasePath);
+                this.connectedDb = database;
+            }
+
+            result = ip2loc.IP2Location_get_all(ip);
+        }
+
         return result;
     }
 }
