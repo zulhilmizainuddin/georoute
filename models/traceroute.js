010a30974f5758332f63a369bbda1c62d13271db
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-22 06:28:09 +0800

    Add traceroute wrapper implementation

--- models/traceroute.js
+++ models/traceroute.js
@@ -4,11 +4,58 @@ const readline = require('readline');
 
 class Traceroute extends events.EventEmitter {
     trace(domainName) {
+        const traceroute = spawn('traceroute', ['-q', 1,'-n', domainName]);
 
+        readline.createInterface({
+            input: traceroute.stdout,
+            terminal: false
+        })
+        .on('line', (line) => {
+            const hop = Traceroute.parseHop(line);
+
+            if (hop !== null) {
+                this.emit('hop', hop);
+                console.log(`traceroute hop: ${JSON.stringify(hop)}`);
+            }
+        });
+
+        readline.createInterface({
+            input: traceroute.stderr,
+            terminal: false
+        })
+        .on('line', (line) => {
+            console.log(`traceroute error: ${line}`);
+        });
+
+        traceroute.on('close', (code) => {
+            this.emit('done', code);
+            console.log(`traceroute process exited with code ${code}`);
+        });
     }
 
     static parseHop(hopData) {
+        const regex = '\\s*(\\d+)\\s+(?:([a-zA-Z0-9:.]+)\\s+([0-9.]+\\s+ms)|(\\*))';
+        const parsedData = new RegExp(regex, '').exec(hopData);
 
+        let result = null;
+        if (parsedData !== null) {
+            if (parsedData[4] === undefined) {
+                result = {
+                    hop: parsedData[1],
+                    ip: parsedData[2],
+                    rtt1: parsedData[3]
+                };
+            }
+            else {
+                result = {
+                    hop: parsedData[1],
+                    ip: parsedData[4],
+                    rtt1: parsedData[4]
+                };
+            }
+        }
+
+        return result;
     }
 }
 
