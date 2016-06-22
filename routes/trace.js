b941025347d26a42ffccf714822cab1387b1a6bd
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-22 16:46:58 +0800

    Add input validation at server side

--- routes/trace.js
+++ routes/trace.js
@@ -1,19 +1,12 @@
 const express = require('express');
+const validator = require('validator');
+
 const router = express.Router();
 
-const Tracert = require('../models/tracert');
-
 router.post('/', (req, res, next) => {
-    try {
-        const tracer = new Tracert();
-
-        tracer.trace(req.body.domainName);
-        tracer.on('hop', (hop) => {
-            /*console.log(JSON.stringify(hop));*/
-        })
-    }
-    catch (err) {
-        console.log(err);
+    if (!validator.isURL(req.body.domainName) && !validator.isIP(req.body.domainName)) {
+        res.sendStatus(400);
+        return;
     }
 
     res.sendStatus(200);
