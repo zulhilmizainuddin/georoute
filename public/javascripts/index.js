5123947438765ddd117263554d28cc24e6ff27a1
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-29 12:31:08 +0800

    Draw polylines between hops

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
             if (status === 'success') {
                 var destinationData = null;
                 var hopData = [];
+                var polylinePoints  = null;
+                var polylines = null;
 
                 var socket = io('/' + data.pid);
                 socket
@@ -44,10 +46,23 @@ $(document).ready(function() {
                                 'Longitude: ' + data.longitude + '<br>';
 
                             marker.bindPopup(popup);
+
+                            polylinePoints = hopData.map(function(data) {
+                                return new L.LatLng(data.latitude, data.longitude);
+                            });
+
+                            if (polylines !== null) map.removeLayer(polylines);
+
+                            polylines = new L.Polyline(polylinePoints);
+
+                            map.addLayer(polylines);
+                            map.fitBounds(polylines.getBounds());
                         }
                     })
                     .on('done', function () {
                         if (hopData[hopData.length -1].ip !== destinationData.ip) {
+                            hopData.push(destinationData);
+
                             var marker = L.marker([destinationData.latitude, destinationData.longitude]).addTo(map);
 
                             var popup =
@@ -59,6 +74,17 @@ $(document).ready(function() {
                                 'Longitude: ' + destinationData.longitude + '<br>';
 
                             marker.bindPopup(popup);
+
+                            polylinePoints = hopData.map(function(data) {
+                                return new L.LatLng(data.latitude, data.longitude);
+                            });
+
+                            if (polylines !== null) map.removeLayer(polylines);
+
+                            polylines = new L.Polyline(polylinePoints);
+
+                            map.addLayer(polylines);
+                            map.fitBounds(polylines.getBounds());
                         }
 
                         socket.disconnect();
