1b6f284a84b9fc4e3d0bba5adbb335308ec6b0e3
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-05 05:39:37 +0800

    Use bower for front end dependencies

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRoutes);
 app.use('/trace', traceRoutes);
