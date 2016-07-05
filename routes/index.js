67ef9b7a22b2afece806ca34c90bc46274821639
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-05 10:52:19 +0800

    Refactor out css code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Georoute' });
+  res.render('index', {
+    title: 'Georoute',
+    year: '2016'
+  });
 });
 
 module.exports = router;
