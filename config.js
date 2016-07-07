c110668a60c9dcf7fb34b312e4b2c28b2875bbdd
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-08 06:19:20 +0800

    Refactor config file

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-
-config = {};
-
-config.ipv4db = 'IP2LOCATION-LITE-DB5.BIN';
-config.ipv6db = 'IP2LOCATION-LITE-DB5.IPV6.BIN';
-
-config.sessionSecret = 'himitsu';
+const config = {
+    ipv4db: 'IP2LOCATION-LITE-DB5.BIN',
+    ipv6db: 'IP2LOCATION-LITE-DB5.IPV6.BIN',
+    sessionSecret: 'himitsu'
+};
 
 module.exports = config;
