24132479a4b0c6711e8e4828d1919c6aa49154ec
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-08 07:50:41 +0800

    Add ip2location disclaimer and refactoring

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.render('index', {
     title: 'georoute',
-    year: '2016'
+    domainInputPlaceholder: 'domain name or ip address',
+    traceButtonText: 'trace',
+    hopsTableHeader: 'hops',
+    year: '2016',
+    disclaimer: 'This site or product includes IP2Location LITE data available from <a href="http://lite.ip2location.com">http://lite.ip2location.com</a>.'
   });
 });
 
