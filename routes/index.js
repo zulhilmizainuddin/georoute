31c8fd5cd13d5a1e19ead3a880df797686c2d02c
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-07-13 18:37:15 +0800

    Add link to github

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', (req, res, next) => {
     traceButtonText: 'trace',
     hopsTableHeader: 'hops',
     year: '2016',
+    github: '<a href="https://github.com/zulhilmizainuddin/georoute">georoute</a>',
     disclaimer: 'This site or product includes IP2Location LITE data available from <a href="http://lite.ip2location.com">http://lite.ip2location.com</a>.'
   });
 });
