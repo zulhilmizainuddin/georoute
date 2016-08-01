8c277e15ddd4ea764fa96cb1d58b0766befe1cca
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-08-02 06:43:12 +0800

    Modified casing

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,17 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'georoute',
-    description: 'traceroute on a map',
+    title: 'Georoute',
+    heading: 'georoute',
+    description: 'Traceroute on a map',
     keywords: 'traceroute, tracert, geoip, geolocation, map',
     author: 'Zulhilmi Mohamed Zainuddin',
     homepage: 'http://georoute.tech',
     githubRepository: 'https://github.com/zulhilmizainuddin/georoute',
     githubLinkText: 'View on GitHub',
-    domainInputPlaceholder: 'domain name or ip address',
-    traceButtonText: 'trace',
-    hopsTableHeader: 'hops',
+    domainInputPlaceholder: 'Domain name or ip address',
+    traceButtonText: 'Trace',
+    hopsTableHeader: 'Hops',
     year: '2016',
     disclaimer: 'This site or product includes IP2Location LITE data available from <a href="http://lite.ip2location.com">http://lite.ip2location.com</a>.'
   });
